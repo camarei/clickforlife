984ba5f3b1f013617eb0600d70bee0b3b13b8148
m0rt15 <ksbomj@gmail.com>
2014-02-17 20:10:29 +0200

    Right model description

--- application/models/authentification.php
+++ application/models/authentification.php
@@ -5,14 +5,25 @@ class Authentification extends CI_Model {
     public function construct() {
         parrent::__construct();
     }
-    
-    public function login(/* $username, $password */) {
+
+    /*
+    * Method login user into the system
+    *
+    * @param stirng $username User name
+    * @param string $password User password
+    * @return array
+    */
+    public function login($username, $password) {
+
+        // Query builder
         $this->db->select('*');
         $this->db->from('users');
         $this->db->where('username',$username);
         $this->db->where('password',$password);
         $this->db->limit(1);
+        //--
         
+        // Return one user record as assoc array
         return $this->db->get()->row_array();
         
     }
