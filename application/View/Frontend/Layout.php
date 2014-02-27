01e3c8c07c86ceda6d385a9ccbc2c5d9260c3165
Sergei <spluiyko@gmail.com>
2014-02-27 20:41:38 +0200

    repair reg page

--- application/View/Frontend/Layout.php
+++ application/View/Frontend/Layout.php
@@ -8,4 +8,9 @@ abstract class View_Frontend_Layout extends View_Base
 	public function logout_user() {
 		return base_url('logout');
 	}
+
+	public function register_user_button_url() {
+		return base_url('registration/method');
+	}
+
 }
